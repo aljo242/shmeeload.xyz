718992e295cc283670c82f859e7ce124518aef9d
aljo242 <amj92@pitt.edu>
2021-02-15 17:58:00 -0500

    ip host selection

--- main.go
+++ main.go
@@ -6,17 +6,21 @@ import (
 	"net"
 	"net/http"
 	"os"
-	
+	"strings"
+
 	"github.com/glendc/go-external-ip"
 )
 
 const (
-	// Port of the HTTP Server
-	Port = "80"
+	DefaultPort = "80"
+
+	DefaultHost = "localhost"
 )
 
 var (
-	
+	// Port of the HTTP Server
+	Port = "80"
+
 	// Host name of the HTTP Server
 	Host = "192.168.1.18"
 )
@@ -38,7 +42,7 @@ func (this webServer) ServeHTTP(w http.ResponseWriter, r *http.Request) {
 func getExternalIP() (string, error) {
 	// create the default consensus
 	// using the default configuratoin and no logger
-	consensus := externalip.DefaultConsensus(nil,nil)
+	consensus := externalip.DefaultConsensus(nil, nil)
 	// Get my IP
 	// which is never <nil> when err is <nil>
 	extIP, err := consensus.ExternalIP()
@@ -49,54 +53,84 @@ func getExternalIP() (string, error) {
 	return extIP.String(), nil
 }
 
-func getHostInfo() {
+func getHostInfo() map[int]string {
+	m := make(map[int]string)
+
 	name, err := os.Hostname()
 	if err != nil {
 		log.Fatal("Error getting Hostname : ", err)
-		return
+		return m
 	}
 	fmt.Printf("Hostname: %v\n", name)
 
 	ifaces, err := net.Interfaces()
 	if err != nil {
 		log.Fatal("Error getting net interfaces : ", err)
-		return
-	}
-
-	for _, i := range ifaces {
-		addrs, err := i.Addrs()
-		if err != nil {
-			log.Fatal("Error getting interface addresses: ", err)
-			return
-		}
-
-		for _, addr := range addrs {
-			var ip net.IP // IP address
-			switch v := addr.(type) {
-			case *net.IPNet:
-				ip = v.IP
-			case *net.IPAddr:
-				ip = v.IP
-			}
-
-			// TODO more with the IP address
-			fmt.Printf("Interface: %v\n", i)
-			fmt.Printf("IP address: %v\n", ip)
-		}
+		return m
 	}
 
 	extIP, err := getExternalIP()
 	if err != nil {
 		log.Fatal("Error getting external IP address: ", err)
-		return
-	} 
-
+		return m
+	}
 	fmt.Printf("External IP address: %v\n", extIP)
-	// Host = extIP
+
+	m[0] = DefaultHost
+	index := 1
+
+	for _, i := range ifaces {
+		addrs, err := i.Addrs()
+		if err != nil {
+			log.Fatal("Error getting interface addresses: ", err)
+			return m
+		}
+
+		if !strings.Contains(i.Name, "lo") {
+			for _, addr := range addrs {
+				var ip net.IP // IP address
+				switch v := addr.(type) {
+				case *net.IPNet:
+					ip = v.IP
+				case *net.IPAddr:
+					ip = v.IP
+				}
+
+				m[index] = ip.String()
+				index++
+			}
+		}
+	}
+
+	return m
+}
+
+func selectHost(ipMap map[int]string) string {
+	fmt.Printf("Choose a host to use:\n")
+	for ind, ip := range ipMap {
+		fmt.Printf("%d\t%v\n", ind, ip)
+	}
+
+	var userInd int = 0
+	_, err := fmt.Scanln(&userInd)
+	if err != nil {
+		log.Fatal("Error scanning user input: ", err)
+		return ""
+	}
+
+	ret, ok := ipMap[userInd]
+	if !ok {
+		log.Fatal("Error getting ip from ip map: ", err)
+		return ""
+	}
+
+	return ret
 }
 
 func main() {
-	getHostInfo()
+	m := getHostInfo()
+	Host := selectHost(m)
+	fmt.Printf("Chose Host: %v\n", Host)
 
 	web := webServer{
 		name:        "Demo Web Server",
@@ -105,7 +139,7 @@ func main() {
 	}
 
 	addr := Host + ":" + Port
-	fmt.Printf("Serving to %v\n", addr)
+	fmt.Printf("Serving to %v...\n", addr)
 
 	err := http.ListenAndServe(addr, web)
 	if err != nil {
