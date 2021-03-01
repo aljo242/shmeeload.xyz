8a582dfc3e3256ca3a94d77f8768e8a58b17ad36
aljo242 <amj92@pitt.edu>
2021-03-01 10:28:37 -0500

    ff 3.1.21

--- web_res/src/chat.ts
+++ web_res/src/chat.ts
@@ -1,4 +1,35 @@
 let signInName : string = "anon";
+const DEFAULT_NAME : string = "anon";
+
+function convertArrayBufferToNumber(buffer: ArrayBuffer) {
+    const bytes = new Uint8Array(buffer);
+    const dv = new DataView(bytes.buffer);
+    return dv.getUint16(0, true);
+}
+
+class User {
+    userName: string;
+    conn: WebSocket;
+
+    constructor(name: string, conn: WebSocket) {
+        console.log("Creating new User...")
+        console.log(this)
+        this.userName = name;
+        this.conn = conn;
+
+        this.signIn();
+    }
+
+    signIn() {
+        const signInMessage = this.userName + " signed in!";
+        this.conn.send(signInMessage);
+        console.log(signInMessage);
+
+        let item = document.createElement("div");
+        item.innerHTML = "<b>Connection closed.</b>";
+        appendLog(item);
+    }
+}
 
 function openPopUpForm() {
     let form = document.getElementById("popUpForm");
@@ -18,13 +49,6 @@ function closePopUpForm() {
     form.style.display = "none";
 }
 
-function signIn() {
-    let userName = document.getElementById("chatname")! as HTMLInputElement;
-    signInName = userName.value;
-    console.log(signInName + " signed in!");
-    closePopUpForm();
-}
-
 //Since you know what type you are expecting, but the type 
 //system can't, to get this to work, you have to tell Typescript 
 //what type of element you expect to be selecting. You would do that 
@@ -34,12 +58,6 @@ function signIn() {
 //const inputElement = document.getElementById("food-name-val") as HTMLInputElement;
 
 
-
-function signInDefault() {
-    console.log(signInName + " signed in!")
-    closePopUpForm();
-}
-
 window.onclick = (event : MouseEvent) => {
     let modal = document.getElementById("popUpForm")!;
     if (event.target == modal) {
@@ -47,32 +65,23 @@ window.onclick = (event : MouseEvent) => {
     }
 }
 
+function appendLog(item : HTMLDivElement) {
+    let log = document.getElementById("log")!;
+    const doScroll = log.scrollTop > log.scrollHeight - log.clientHeight - 1;
+    log.appendChild(item);
+    if (doScroll) {
+        log.scrollTop = log.scrollHeight - log.clientHeight;
+    }
+    console.log(log);
+
+}
+
+
 window.onload = () => {
     openPopUpForm()
     let conn: WebSocket;
+    let user: User;
     let msg = document.getElementById("msg")! as HTMLInputElement;
-    let log = document.getElementById("log")!;
-
-    function appendLog(item : HTMLDivElement) {
-        let doScroll = log.scrollTop > log.scrollHeight - log.clientHeight - 1;
-        log.appendChild(item);
-        if (doScroll) {
-            log.scrollTop = log.scrollHeight - log.clientHeight;
-        }
-    }
-
-    document.getElementById("form")!.onsubmit = () => {
-        if (!conn) {
-            return false;
-        }
-        if (!msg.value) {
-            return false;
-        }
-        let messageWithName = signInName + ": " + msg.value;
-        conn.send(messageWithName);
-        msg.value = "";
-        return false;
-    };
 
     if (window["WebSocket"]) {
         conn = new WebSocket("ws://" + document.location.host + "/ws");
@@ -93,6 +102,34 @@ window.onload = () => {
         let item = document.createElement("div");
         item.innerHTML = "<b>Your browser does not support WebSockets.</b>";
         appendLog(item);
+        return
     }
+
+    let signInButton = document.getElementById("signInButton")!;
+    signInButton.onclick = () => {
+        let userName = document.getElementById("chatname") as HTMLInputElement;
+        if (userName.value != "") {
+            user = new User(userName.value, conn);
+            closePopUpForm(); 
+        } else {
+            console.log("USER INPUT ERROR");
+            user = new User(DEFAULT_NAME, conn);
+            closePopUpForm(); 
+        }
+    }
+
+    let msgForm = document.getElementById("signInButton")!;
+    msgForm.onsubmit = () => {
+        if (!conn) {
+            return false;
+        }
+        if (!msg.value) {
+            return false;
+        }
+        const messageWithName = user.userName + ": " + msg.value;
+        conn.send(messageWithName);
+        msg.value = "";
+        return false;
+    };
 };
 
