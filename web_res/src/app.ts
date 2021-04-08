7a84564ad28d54f91cbae96b32a00e15177315b7
aljo242 <amj92@pitt.edu>
2021-04-08 19:47:16 -0400

    working color scheme switching

--- web_res/src/app.ts
+++ web_res/src/app.ts
@@ -1,6 +1,43 @@
-import * as colors from "./color_schemes";
+// type alias
+type Color = string;
+const black: Color = "#000000"
 
-console.log(colors.scheme1)
+class ColorScheme {
+    article_background: Color = black;
+    nav_background: Color = black;
+    nav_header: Color = black;
+    body_background: Color = black;
+    hr: Color = black;
+
+    a_link: Color = black;
+    a_visited: Color = black;
+    a_hover: Color = black;
+    a_active: Color = black;
+
+    main_text: Color = black;
+}
+
+let scheme1 = new ColorScheme;
+scheme1.article_background = "#f4a261";
+scheme1.nav_background = "#264653";
+scheme1.nav_header = "#e76f51";
+scheme1.body_background = black;
+scheme1.hr = black;
+scheme1.a_link = "#e9c46a";
+scheme1.a_visited = "e9c46a";
+scheme1.a_hover = "e9c46a";
+scheme1.a_active = "e9c46a";
+
+let scheme2 = new ColorScheme;
+scheme2.article_background = "#f0e68c";
+scheme2.nav_background = "#adff2f";
+scheme2.nav_header = "#ffc0cb";
+scheme2.body_background = black;
+scheme2.hr = black;
+scheme2.a_link = "#f700ff";
+scheme2.a_visited = "f700ff";
+scheme2.a_hover = "ffd038";
+scheme2.a_active = "ffff00";
 
 // Register Service Worker
 if ("serviceWorker" in navigator) {
@@ -18,10 +55,7 @@ if ("serviceWorker" in navigator) {
 
 const msg = "Hello web"
 
-const homeButton = document.getElementById("secretButton")!
-homeButton.onclick = () => {
-    printMessage("Button Pushed!");
-};
+
 
 
 
@@ -30,4 +64,34 @@ function printMessage(msg: string) {
 }
 
 
-printMessage(msg)
+function applyColorScheme(scheme: ColorScheme) {
+  console.log(scheme)
+  let header = document.getElementById("header")!;
+  header.style.backgroundColor = scheme.nav_header;
+  let nav = document.getElementById("nav")!;
+  nav.style.backgroundColor = scheme.nav_background;
+  let mainArticleBody = document.getElementById("mainArticleBody")!;
+  mainArticleBody.style.backgroundColor = scheme.article_background
+  let navLinks = document.getElementsByTagName("a")!;
+  for (let item  of navLinks) {
+    console.log(item.style)
+    item.style.color = scheme.a_link;
+  }
+}
+
+const colorSchemes = [scheme1, scheme2];
+
+
+function applyRandomColorScheme() {
+  const random = Math.floor(Math.random() * colorSchemes.length);
+  applyColorScheme(colorSchemes[random]);
+}
+
+
+const homeButton = document.getElementById("secretButton")!
+homeButton.onclick = () => {
+    printMessage("Button Pushed!");
+    applyRandomColorScheme();
+};
+
+
